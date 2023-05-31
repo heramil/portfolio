a1e725a0e1fba278b53cef11c56e4d87f1ac6ee5
Prakash Heramil <ph@Prakashs-MacBook-Air.local>
2023-05-31 16:55:25 -0700

    changed development connection value

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,17 +1,10 @@
 const path = require("path")
 require("dotenv").config()
 
-const { 
-	DATABASE_URL_DEVELOPMENT,
-	DATABASE_URL_TEST,
-	DATABASE_URL_STAGING,
-	DATABASE_URL_PRODUCTION
-} = process.env
-
 module.exports = {
 	development: {
 		client: "postgresql",
-		connection: DATABASE_URL_DEVELOPMENT,
+		connection: process.env.DATABASE_URL_DEVELOPMENT,
 		migrations: {
 			directory: path.join(__dirname, "src", "db", "migrations"),
 		},
