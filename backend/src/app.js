87712424e82cd1fa4b197fcc4ab8674e0837c9d2
Prakash Heramil <ph@Prakashs-MacBook-Air.local>
2023-06-01 07:24:35 -0700

    changed app.js and api.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ const projectsRouter = require("./projects/projects.router");
 
 app.use(cors());
 app.use(express.json());
-app.use("/projects", projectsRouter);
+
+app.use("/", projectsRouter);
 
 module.exports = app;
--- frontend/src/utils/api/api.js
+++ frontend/src/utils/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5001"
+const API_BASE_URL = process.env.API_BASE_URL || "https://test-portfolio-zqvg.onrender.com"
 
 async function fetchJson(url, options, onCancel) {
   try {
